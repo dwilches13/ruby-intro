fb06e447925cd8934308bf20166db2f1d47151b0
dwilches13 <daniel.wilches@kellogg.northwestern.edu>
2022-01-25 17:38:34 +0000

    Loops

--- labs/5-loops.rb
+++ labs/5-loops.rb
@@ -9,3 +9,8 @@
 ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 
+for card in ranks
+    for suit in suits
+        puts "#{card} of #{suit}"
+    end
+end
